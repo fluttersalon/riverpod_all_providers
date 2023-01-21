f2a2e75ba5ab8275a50999d77266e65e2c8d9290
sakushin <sakushin.udemy@gmail.com>
2023-01-22 08:07:45 +0900

    初期状態

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
