7791b8439b946748422fe7cfb8061f437e154c2b
sakushin <sakushin.udemy@gmail.com>
2023-02-06 06:17:17 +0900

    AsyncNotifierProviderを使用

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 part 'main.g.dart';
-
-@riverpod
-class Counter extends _$Counter {
-  @override
-  int build() => 0;
-
-  void increase() => state++;
-}
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -31,9 +25,34 @@ class MyApp extends StatelessWidget {
   }
 }
 
+@riverpod
+String githubUrl(_) =>
+    'https://api.github.com/search/repositories?q=[keyword]&page=[page]';
+
+@riverpod
+Future<String> repositoryTitle(RepositoryTitleRef ref,
+    {required int page, required String keyword}) async {
+  final url = ref
+      .read(githubUrlProvider)
+      .replaceAll('[keyword]', keyword)
+      .replaceAll('[page]', page.toString());
+
+  print('url: $url');
+
+  final client = http.Client();
+  final result = await client.get(Uri.parse(url));
+  final map = json.decode(result.body) as Map<String, dynamic>;
+
+  final title = map['items'][0]['name'];
+
+  return title;
+}
+
 class MyHomePage extends ConsumerWidget {
   MyHomePage({super.key, required this.title});
   final String title;
+
+  int _page = 1;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -48,15 +67,24 @@ class MyHomePage extends ConsumerWidget {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              ref.watch(counterProvider).toString(),
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            ref
+                .watch(repositoryTitleProvider(page: _page, keyword: 'flutter'))
+                .when(
+                  loading: () => const CircularProgressIndicator(),
+                  error: (error, stack) => const Text('error'),
+                  data: (title) => Text(
+                    title,
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => ref.read(counterProvider.notifier).increase(),
+        onPressed: () async {
+          _page++;
+          ref.invalidate(repositoryTitleProvider);
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
